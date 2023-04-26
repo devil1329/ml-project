b9e779b0f7d4bf610b986a26873ea3444d449392
suvi2913 <server.visu143@gmail.com>
2023-04-26 15:05:37 +0530

    Data Ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,8 +1,8 @@
 import logging
 import os
-from datetime import datatime
+from datetime import datetime
 
-LOG_FILE = f"{datatime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
 
