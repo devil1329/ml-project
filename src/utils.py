eb5fdf7cc82445bebe685f94e9cb607702aec730
suvi2913 <server.visu143@gmail.com>
2023-04-27 16:00:39 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,10 @@ import numpy as np
 import pandas as pd
 import pickle
 
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
+from src.logger import logging
 
 def save_object(file_path, obj):
         try:
@@ -18,3 +21,26 @@ def save_object(file_path, obj):
 
         except Exception as e:
                 raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+        try:
+                report = {}
+                logging.info("Evaluation Started")
+                for i in range(len(list(models))):
+                        model = list(models.values())[i]
+
+                        model.fit(X_train, y_train) # train model
+                        y_train_pred = model.predict(X_train)
+                        y_test_pred = model.predict(X_test)
+
+                        # comparing r2_score of both the predicted values
+                        train_model_score = r2_score(y_train, y_train_pred)
+                        test_model_score = r2_score(y_test, y_test_pred)
+
+                        report[list(models.keys())[i]] = test_model_score
+                
+                logging.info("Evaluation  completed")
+                return report
+
+        except Exception as e:
+                raise CustomException(e,sys)
